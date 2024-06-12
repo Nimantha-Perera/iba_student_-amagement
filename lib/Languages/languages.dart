2a7df364cae7214fd8d397256e12759e7789d74d
Nimantha Perera <126785445+Nimantha-Perera@users.noreply.github.com>
2024-06-12 11:34:55 +0530

    Change Version

---
+++ lib/Languages/languages.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+class LanguageManager {
+  static late SharedPreferences _prefs; // SharedPreferences instance
+  static String? selectedLanguage;
+  static final Map<String, Map<String, String>> translations = {
+    'English': {
+      'Who Are You?': 'Who Are You?',
+      'I\'M A STUDENT': 'I\'M A STUDENT',
+      'I\'M AN IBA CAMPUS STAFF': 'I\'M AN IBA CAMPUS STAFF',
+    },
+    'සිංහල': {
+      'Who Are You?': 'ඔබ කවුද?',
+      'I\'M A STUDENT': 'මම ශිෂ්‍යයෙකි',
+      'I\'M AN IBA CAMPUS STAFF': 'මම කාර්‍ය මන්ඩලයේ සාමාජිකයෙකි',
+      'Login your student portal': 'ඔබේ ශිෂ්‍ය ද්වාරය ලොගින් කරන්න',
+      'IBA Campus already registered students only' : 'IBA කැම්පස් දැනටමත් ලියාපදිංචි සිසුන් පමණි',
+      'Index Number':'Index අංකය',
+      'Password':'රහස්‍ය අංකය',
+      'Keep me signed in':'මාව පුරනය වී තබා ගන්න',
+      'LOGIN':'පුරනය වන්න',
+      'Forgot your password':'වෙනස් කරන්න?',
+    },
+    'தமிழ்': {
+      'Who Are You?': 'நீ யார்?',
+      'I\'M A STUDENT': 'நான் மாணவர்',
+      'I\'M AN IBA CAMPUS STAFF': 'நான் ஐபிஏ நிறுவனத்தின் அமைப்பு உதவியாளர்',
+      
+    },
+  };
+
+  static Future<void> init() async {
+    _prefs = await SharedPreferences.getInstance(); // Initialize SharedPreferences
+    selectedLanguage = _prefs.getString('selectedLanguage') ?? 'English'; // Retrieve saved language or default to English
+  }
+
+  static String getText(String key) {
+    final currentLanguage = selectedLanguage ?? 'English';
+    return translations[currentLanguage]?[key] ?? key;
+  }
+
+  static Future<void> changeLanguage(BuildContext context, String newLanguage) async {
+    selectedLanguage = newLanguage;
+    _prefs.setString('selectedLanguage', newLanguage); // Save selected language in SharedPreferences
+    // Notify the widgets that the language has changed
+    // For example, you can use Provider, InheritedWidget, or a similar solution
+    // and notify the consumers here.
+  }
+}
---
+++ lib/Screens/selected_screen.dart
@@ -0,0 +1,153 @@
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:iba_app/Languages/languages.dart';
+// Import the LanguageManager class
+
+class SelectedScreen extends StatefulWidget {
+  const SelectedScreen({Key? key}) : super(key: key);
+
+  @override
+  _SelectedScreenState createState() => _SelectedScreenState();
+}
+
+class _SelectedScreenState extends State<SelectedScreen> {
+  String? _selectedValue;
+  final List<String> _dropdownItems = ['English', 'සිංහල', 'தமிழ்'];
+
+  String _getText(String key) {
+    return LanguageManager.getText(key); // Utilize the LanguageManager to get translated text
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: SafeArea(
+          child: Stack(
+            children: [
+              Container(
+                alignment: Alignment.topRight,
+                child: Padding(
+                  padding: const EdgeInsets.all(20.0),
+                  child: Container(
+                    height: 33,
+                    decoration: BoxDecoration(
+                      color: Color.fromARGB(255, 207, 207, 207),
+                      borderRadius: BorderRadius.circular(20.0),
+                      border: Border.all(
+                        color: Color.fromARGB(255, 161, 161, 161),
+                        width: 1.0,
+                      ),
+                    ),
+                    child: Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: DropdownButton<String>(
+                        icon: FaIcon(
+                          FontAwesomeIcons.globe,
+                          size: 15,
+                          color: const Color.fromARGB(255, 0, 0, 0),
+                        ),
+                        hint: Padding(
+                          padding: const EdgeInsets.only(right: 10),
+                          child: Text(
+                            _getText('Language'),
+                            style: TextStyle(fontSize: 12),
+                          ),
+                        ),
+                        value: _selectedValue,
+                        onChanged: (String? newValue) {
+                          setState(() {
+                            _selectedValue = newValue;
+                            LanguageManager.selectedLanguage = newValue; // Update selected language in LanguageManager
+                          });
+                        },
+                        underline: Container(),
+                        items: _dropdownItems
+                            .map<DropdownMenuItem<String>>((String value) {
+                          return DropdownMenuItem<String>(
+                            value: value,
+                            child: Text(
+                              value,
+                              style: TextStyle(fontSize: 12),
+                            ),
+                          );
+                        }).toList(),
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+              Container(
+                margin: EdgeInsets.only(top: 50),
+                child: Padding(
+                  padding: const EdgeInsets.all(30.0),
+                  child: Text(
+                    _getText('Who Are You?'),
+                    style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
+                  ),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(20.0),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Container(
+                      width: double.infinity,
+                      child: Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: ElevatedButton(
+                          style: ButtonStyle(
+                            backgroundColor: MaterialStateProperty.all(Colors.black),
+                            overlayColor: MaterialStateProperty.all(Color.fromARGB(55, 255, 255, 255)),
+                            minimumSize: MaterialStateProperty.all(Size(200, 50)),
+                            shape: MaterialStateProperty.all(
+                              RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(10),
+                              ),
+                            ),
+                          ),
+                          onPressed: () {
+                            Navigator.pushNamed(context, '/student_login');
+                          },
+                          child: Text(
+                            _getText('I\'M A STUDENT'),
+                            style: TextStyle(color: Colors.white),
+                          ),
+                        ),
+                      ),
+                    ),
+                    SizedBox(height: 10),
+                    Container(
+                      width: double.infinity,
+                      child: Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: ElevatedButton(
+                          style: ButtonStyle(
+                            minimumSize: MaterialStateProperty.all(Size(200, 50)),
+                            backgroundColor: MaterialStateProperty.all(Colors.black),
+                            overlayColor: MaterialStateProperty.all(Color.fromARGB(55, 255, 255, 255)),
+                            shape: MaterialStateProperty.all(
+                              RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(10),
+                              ),
+                            ),
+                          ),
+                          onPressed: () {},
+                          child: Text(
+                            _getText('I\'M AN IBA CAMPUS STAFF'),
+                            style: TextStyle(color: Colors.white),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
